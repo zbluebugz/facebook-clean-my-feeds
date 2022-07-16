b3afa3f5eb7f6493d178ff37fd91b8c4ca76700e
zbluebugz <51784903+zbluebugz@users.noreply.github.com>
2022-07-16 06:55:45 +0100

    Update main.js

--- greasyfork-release/main.js
+++ greasyfork-release/main.js
@@ -3,7 +3,7 @@
 // @description  Hide Sponsored and Suggested posts in FB's News Feed, Groups Feed, Watch Videos Feed and Marketplace Feed
 // @namespace    https://greasyfork.org/users/812551
 // @supportURL   https://github.com/zbluebugz/facebook-clean-my-feeds/issues
-// @version      3.23
+// @version      3.24
 // @author       zbluebugz (https://github.com/zbluebugz/)
 // @require      https://unpkg.com/idb-keyval@6.0.3/dist/umd.js
 // @match        https://*.facebook.com/*
@@ -13,6 +13,8 @@
 // @run-at       document-start
 // ==/UserScript==
 /*
+    v3.24 :: July 2022
+        Updated detection code for: Sponsored posts in News Feed
     v3.23 :: July 2022
         Updated detection code for: Sponsored posts in News Feed
     v3.22 :: July 2022
@@ -2452,11 +2454,12 @@
         let daText = '';
 
         // -- try the Flex/Order structure
-        let elWrapper = post.querySelector(
-            'span > span > span > a[href^="?"] > span > span[class] > [style*="order"], span > span > span > a[href*="/ads/"] > span > span[class] > [style*="order"], ' +
-            'span > span > span > a[href^="?"] > span > span[class] > [style*="display"], span > span > span > a[href*="/ads/"] > span > span[class] > [style*="display"], ' +
-            'span > span > span > a[href="#"] > span > span[class] > [style*="order"], span > span > span > a[href*="/ads/"] > span > span[class] > [style*="order"], ' +
-            'span > span > span > a[href="#"] > span > span[class] > [style*="display"], span > span > span > a[href*="/ads/"] > span > span[class] > [style*="display"]');
+        let elWrapper = post.querySelector( 'span > span > span > a[href^="?"] > span > span[class] > [style*="order"], ' +
+                                            'span > span > span > a[href="#"] > span > span[class] > [style*="order"], ' + 
+                                            'span > span > span > a[href^="?"] > span > span[class] > [style*="display"], ' + 
+                                            'span > span > span > a[href="#"] > span > span[class] > [style*="display"], '+
+                                            'span > span > span > a[href*="/ads/"] > span > span[class] > [style*="order"], ' +                                            
+                                            'span > span > span > a[href*="/ads/"] > span > span[class] > [style*="display"]');
         if (elWrapper) {
             // -- found a regular post structure
             let arrText = [];
@@ -2481,7 +2484,9 @@
         }
         else {
             // -- try the non-Flex/Order structure
-            elWrapper = post.querySelector('span > span > span > a[href="#"] > span > span[class] > [class], span > span > span > a[href*="/ads/"] > span > span[class] > [class]');
+            elWrapper = post.querySelector( 'span > span > span > a[href^="?"] > span > span[class] > [class], ' +
+                                            'span > span > span > a[href="#"] > span > span[class] > [class], ' +
+                                            'span > span > span > a[href*="/ads/"] > span > span[class] > [class] ' );
             if (elWrapper) {
                 // -- found a regular post structure (Portugese, Italian)
                 daText = '';
