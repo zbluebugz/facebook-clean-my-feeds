e82680fa37a07d93d5470e0515c8e7464392be43
zbluebugz <51784903+zbluebugz@users.noreply.github.com>
2022-10-19 22:55:41 +0100

    Update sponsored-filter-builder.js

--- uBO-sponsored-filter/sponsored-filter-builder.js
+++ uBO-sponsored-filter/sponsored-filter-builder.js
@@ -36,21 +36,24 @@
                 let daText = el.textContent;
                 // - fb sometimes use ASCII code 160 for whitespace ... and trim it
                 daText = daText.replaceAll(String.fromCharCode(160), String.fromCharCode(32)).trim();
-                // - see if is sponsored (based on text not having a number - will get the odd false positive - e.g. "just now")
-                //let 
+                // - see if is sponsored (based on text not having a number - will get the odd false positive - e.g. "just now", "join", "apply now")
                 if (!pattern.test(daText)) {
-                    // - found sponsored (with 99% confidence)
-                    //let elPostSVGUse = document.querySelector(`span[id] > span > span > a[href] span > span > svg > use[*|href="#${el.id}"]:not([href])`);
+                    // - found a possible sponsored post with 75% confidence, let's improve on that ...
                     let elPostSVGUse = document.querySelector(`svg > use[*|href="#${el.id}"]:not([href])`);
-                    if (elPostSVGUse) {
-                        // get width of the USE's grand parent element
-                        let bcr = elPostSVGUse.closest('span').getBoundingClientRect();
-                        let intWidth = parseInt(bcr.width, 10); // rounds down
-                        let roundWidth = Math.round(bcr.width); // rounds up/down
-                        // console.info('----***:', daText, bcr.width, intWidth, roundWidth, elPostSVGUse);
-                        if (intWidth > 0) {
-                            arWidths.push(intWidth);
-                            arWidths.push(roundWidth);
+                    // test if nearest A's href is "#".
+                    let aTag = elPostSVGUse.closest('a');
+                    if (aTag.href.indexOf("#") >= 0) {
+                        if (elPostSVGUse) {
+                            // get width of the USE's grand parent element
+                            let grandPar = elPostSVGUse.parentElement.parentElement; // either div or span.
+                            let bcr = grandPar.getBoundingClientRect();
+                            let intWidth = parseInt(bcr.width, 10); // rounds down
+                            let roundWidth = Math.round(bcr.width); // rounds up/down
+                            //console.info('----***:', daText, bcr.width, intWidth, roundWidth, grandPar);
+                            if (intWidth > 0) {
+                                arWidths.push(intWidth);
+                                arWidths.push(roundWidth);
+                            }
                         }
                     }
                 }
@@ -97,10 +100,10 @@
             const filterComment2 = '! FB - sponsored text (non-obfuscated)\n';
 
             rule1 = filterComment1 + `facebook.com##${postContainer} > div:not([class]) span > span > span > a[href="#"] > span${upward}${highlight}\n` +
-                                     `facebook.com##${postContainer} > div:not([class]) span > span > span > a[href*="/ads/"] > span${upward}${highlight}`;
-            
+                `facebook.com##${postContainer} > div:not([class]) span > span > span > a[href*="/ads/"] > span${upward}${highlight}`;
+
             rule2 = filterComment2 + `facebook.com##${postContainer} > div:not([class]) span > span > span > a[href="#"] > span${upward}${live}\n` +
-                                     `facebook.com##${postContainer} > div:not([class]) span > span > span > a[href*="/ads/"] > span${upward}${live}`;
+                `facebook.com##${postContainer} > div:not([class]) span > span > span > a[href*="/ads/"] > span${upward}${live}`;
 
         }
     }
@@ -110,7 +113,7 @@
         nfDummyRule = `facebook.com##${postContainer} > div div[data-0]:remove()`;
         nfSuggestionRule = `facebook.com##${postContainer} > div > div > div > div > div > div > div > div > div > div > div > div:nth-of-type(2) > div > div:nth-of-type(1) > div > div > div > div > span:not(>*)`;
         gfSuggestionRule = `facebook.com##div[role="feed"] > div div[aria-posinset] > div > div > div > div > div > div > div > div:nth-of-type(2) h3 > div > span ~ span > span > div > div`;
-        
+
         let logText = 'The following filter rules are for use in uBlock Origin.\n';
         logText += '========================================================\n\n';
         logText += 'News Feed Sponsored posts\n';
@@ -129,19 +132,19 @@
         logText += rule2 + '\n\n\n';
 
         if (ECHO_SUGGESTED) {
-        logText += 'News Feed Suggestions/Recommendations filter rule:\n';
-        logText += '--------------------------------------------------\n\n';
-        logText += '! FB - News Feed - remove "dummy" elements helper (helps the news feed suggestion/recommendation rule to work)\n';
-        logText += nfDummyRule + '\n\n';
-        logText += '! FB - News Feed - suggestions / recommendations (TEST MODE)\n';
-        logText += nfSuggestionRule + upward + highlight + '\n\n\n';
-        logText += '! FB - News Feed - suggestions / recommendations (LIVE MODE)\n';
-        logText += nfSuggestionRule + upward + live + '\n\n\n';
-        logText += 'Groups Feed suggestions filter rule:\n\n';
-        logText += '! FB - Groups Feed - suggestions / recommendations (TEST MODE)\n';
-        logText += gfSuggestionRule + ':upward(div[role="feed"] > div)' + highlight + '\n\n\n';
-        logText += '! FB - Groups Feed - suggestions / recommendations (LIVE MODE)\n';
-        logText += gfSuggestionRule + ':upward(div[role="feed"] > div)' + live + '\n\n\n';
+            logText += 'News Feed Suggestions/Recommendations filter rule:\n';
+            logText += '--------------------------------------------------\n\n';
+            logText += '! FB - News Feed - remove "dummy" elements helper (helps the news feed suggestion/recommendation rule to work)\n';
+            logText += nfDummyRule + '\n\n';
+            logText += '! FB - News Feed - suggestions / recommendations (TEST MODE)\n';
+            logText += nfSuggestionRule + upward + highlight + '\n\n\n';
+            logText += '! FB - News Feed - suggestions / recommendations (LIVE MODE)\n';
+            logText += nfSuggestionRule + upward + live + '\n\n\n';
+            logText += 'Groups Feed suggestions filter rule:\n\n';
+            logText += '! FB - Groups Feed - suggestions / recommendations (TEST MODE)\n';
+            logText += gfSuggestionRule + ':upward(div[role="feed"] > div)' + highlight + '\n\n\n';
+            logText += '! FB - Groups Feed - suggestions / recommendations (LIVE MODE)\n';
+            logText += gfSuggestionRule + ':upward(div[role="feed"] > div)' + live + '\n\n\n';
         }
         console.clear();
         console.info(logText);
